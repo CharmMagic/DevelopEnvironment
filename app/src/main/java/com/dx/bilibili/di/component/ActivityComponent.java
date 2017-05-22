9069101fd12920cd2bebc4dcbe1d0e6424206cc0
miserydx <miserydx@163.com>
2017-05-22 17:54:10 +0800

    feature: 框架初始化

---
+++ app/src/main/java/com/dx/bilibili/di/component/ActivityComponent.java
@@ -0,0 +1,38 @@
+package com.dx.bilibili.di.component;
+
+import com.dx.bilibili.di.module.ActivityModule;
+import com.dx.bilibili.ui.test.activity.NewsActivity;
+import com.dx.bilibili.ui.test.activity.TestApiActivity;
+import com.dx.bilibili.ui.test.activity.TestNoBaseActivity;
+import com.dx.bilibili.ui.test.activity.TestNoBaseMvpActivity;
+import com.dx.bilibili.ui.test.activity.ToolbarBehaviorActivity;
+import com.dx.bilibili.di.module.PageModule;
+import com.dx.bilibili.di.scope.PerActivity;
+import com.dx.bilibili.ui.test.activity.ScrollGradientActivity;
+import com.dx.bilibili.ui.test.activity.StatusWithPictureActivity;
+
+import dagger.Component;
+
+/**
+ * Created by jiayiyang on 17/3/23.
+ */
+
+@Component(dependencies = AppComponent.class, modules = {ActivityModule.class, PageModule.class})
+@PerActivity
+public interface ActivityComponent {
+
+    void inject(NewsActivity newsActivity);
+
+    void inject(ToolbarBehaviorActivity toolbarBehaviorActivity);
+
+    void inject(StatusWithPictureActivity statusWithPictureActivity);
+
+    void inject(ScrollGradientActivity scrollGradientActivity);
+
+    void inject(TestApiActivity testApiActivity);
+
+    void inject(TestNoBaseActivity testNoBaseActivity);
+
+    void inject(TestNoBaseMvpActivity testNoBaseMvpActivity);
+
+}
---
+++ app/src/main/java/com/dx/bilibili/model/api/ZhihuApis.java
@@ -0,0 +1,24 @@
+package com.dx.bilibili.model.api;
+
+
+import com.dx.bilibili.di.scope.ApiInfo;
+import com.dx.bilibili.model.bean.HotListBean;
+
+import retrofit2.http.GET;
+import rx.Observable;
+
+/**
+ * Created by yangjiayi on 2017/3/25.
+ * 知乎Apis
+ */
+public interface ZhihuApis {
+
+    String HOST = "http://news-at.zhihu.com/api/4/";
+
+    /**
+     * 热门日报
+     */
+    @GET("news/hot")
+    @ApiInfo(needSigned = false)
+    Observable<HotListBean>  getHotList();
+}
